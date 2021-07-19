c7c14e5dd61549ab7833ef980ef8053259a5f6fb
adnanali000 <adnan.museel123@gmail.com>
2021-07-19 17:37:42 +0500

    counter done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './App.css';
+import {useSelector,useDispatch} from 'react-redux';
+import {incNumber,decNumber} from './actions/index';
 
 function App() {
+  const myState = useSelector((state)=>state.changeTheNumber);
+  const dispatch = useDispatch();
+
   return (
     <div className="container">
         <h1>Increment / Decrement Counter</h1>
         <div className="quantity">
-          <a className="minus" title="Decrement"><span>-</span></a>
-          <input type="text" name="quantity" className = "qinput" value="0" />
-          <a className="plus" title="Increment"><span>+</span></a>
+          <a className="minus"
+          onClick={()=>dispatch(decNumber())}
+          title="Decrement"><span>-</span></a>
+          <input type="text" name="quantity" className = "qinput" value={myState} />
+          <a className="plus"
+          onClick={()=> dispatch(incNumber())}
+          title="Increment"><span>+</span></a>
 
         </div>
     </div>
